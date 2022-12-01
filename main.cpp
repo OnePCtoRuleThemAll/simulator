2ec8f06ef8395112225679baf09fd14d73d5de5a
OnePCtoRuleThemAll <OnePCtoRuleThemAll@users.noreply.github.com>
2022-12-01 15:35:54 +0100

    -openGL library  added to project

--- main.cpp
+++ main.cpp
@@ -2,28 +2,13 @@
 #include "tests/testGeometry/testGeometry.h"
 #include "ui/ui_keeper.h"
 #include "ui/console.h"
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char* argv[]) { 
-	//Geometry2D::Point<int>* point = new Geometry2D::Point<int>(1, 1);
-	//Geometry2D::Point<int>* cpyPoint = new Geometry2D::Point<int>(*point);
-	//Geometry2D::Vector<int>* vector = new Geometry2D::Vector<int>(2, 5);
-	//Geometry2D::Point<int>* startPoint = new Geometry2D::Point<int>(4, 5);
-	//Geometry2D::Point<int>* endPoint = new Geometry2D::Point<int>(3, 1);
-
-	//cout << cpyPoint->mPositionX << endl;
-	//point->mPositionX = 0;
-	//cout << point->mPositionX << endl;
-	//cout << cpyPoint->mPositionX << endl;
-	//
-	//Geometry2D::Line<int>* line = new Geometry2D::Line<int>(*startPoint, *endPoint);
-	//cout << Geometry2D::distancePointToLine(*line, *point) << endl;
-	//
-	//delete point;
-	//delete vector;
-	//delete line;
 
 	ui::UiKeeper keeper;
 	structures::Logger::getInstance().registerConsumer(&keeper);
@@ -43,6 +28,39 @@ int main(int argc, char* argv[]) {
 		keeper.clearLog();
 		run = keeper.readShouldContinue();
 	} while (run);
+
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
 
 	return 0;
 }
