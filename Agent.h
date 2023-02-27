99d7a7e73be493a71e2b781a0cb35b4a0a44bbbb
Andrej-Dusa <ayba1dusa@gmail.com>
2023-02-27 08:40:32 +0100

    -agent changes

--- Agent.h
+++ Agent.h
@@ -13,26 +13,26 @@ public:
 	/// <summary> Assign operator. </summary>
 	/// <param name = "other"> Source objcet of taken properties.</param>
 	/// <returns> Adress of the object. </returns>
-	Agent& operator=(const Agent& other);
+	Agent<T>& operator=(const Agent<T>& other);
 
 	/// <summary>  Assigning object. </summary>
 	/// <param name = "other"> Source objcet of taken properties. </param>
 	/// <returns> Adress of the object. </returns>
-	virtual Agent& assign(const Agent& other) = 0;
+	virtual Agent<T>& assign(const Agent<T>& other) = 0;
 
 	/// <summary> Equals operator. </summary>
 	/// <param name="other">Object to compare with. </param>
 	/// <returns>True if objects are equal. </returns>
-	bool operator==(const Agent& other);
+	bool operator==(const Agent<T>& other);
 
 	/// <summary> Objcet equality. </summary>
 	/// <param name="other">Object to compare with. </param>
 	/// <returns>True if objects are equal. </returns>
-	virtual bool equals(const Agent& other) = 0;
+	virtual bool equals(const Agent<T>& other) = 0;
 
 	virtual void execute() = 0;
 
-	void act();
+	virtual void act() = 0;
 
 	void moveTo(Geometry2D::Point<T>& newPosition);
 
@@ -47,3 +47,51 @@ private:
 
 	Geometry2D::Vector<T>* mDirection;
 };
+
+template<typename T>
+inline Agent<T>::~Agent()
+{
+	delete this->mPosition;
+	delete this->mDirection;
+
+	this->mPosition = nullptr;
+	this->mDirection = nullptr;
+}
+
+template<typename T>
+inline Agent<T>& Agent<T>::operator=(const Agent<T>& other)
+{
+	return this->assign(other);
+}
+
+template<typename T>
+inline bool Agent<T>::operator==(const Agent<T>& other)
+{
+	return this->equals(other);;
+}
+
+template<typename T>
+inline void Agent<T>::moveTo(Geometry2D::Point<T>& newPosition)
+{
+	delete this->mPosition;
+	this->mPosition = *newPosition;
+}
+
+template<typename T>
+inline Geometry2D::Point<T>* Agent<T>::getPosition()
+{
+	return this->mPosition;
+}
+
+template<typename T>
+inline Geometry2D::Vector<T>* Agent<T>::getDirection()
+{
+	return this->mDirection;
+}
+
+template<typename T>
+inline void Agent<T>::setDirection(Geometry2D::Vector<T>& direction)
+{
+	delete this->mDirection;
+	this->mDirection = *direction;
+}
